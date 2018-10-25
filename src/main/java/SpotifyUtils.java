c0144fc68de95edb99afc4998a5ef932be1a5b42
Austin Bristol <bristola@aldenv175.allegheny.edu>
2018-10-25 14:23:39 -0400

    Added basic structure

---
+++ src/main/java/SpotifyUtils.java
@@ -0,0 +1,13 @@
+import com.wrapper.spotify.SpotifyApi;
+import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import com.wrapper.spotify.model_objects.specification.Paging;
+import com.wrapper.spotify.model_objects.specification.PlaylistSimplified;
+import com.wrapper.spotify.requests.data.playlists.GetListOfCurrentUsersPlaylistsRequest;
+
+public class SpotifyUtils {
+
+    public static void main(String[] args) {
+
+    }
+
+}
