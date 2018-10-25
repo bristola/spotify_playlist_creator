42009cbeca534623c19f086fd8cfb10c04706822
Austin Bristol <bristola@aldenv175.allegheny.edu>
2018-10-25 14:02:01 -0400

    Initial commit of gradle java application

---
+++ src/main/java/App.java
@@ -0,0 +1,10 @@
+/**
+    This is the drive class for starting up the program
+**/
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+    
+}
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,11 @@
+/**
+    JUnit test suite for application
+**/
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test public void test1() {
+        assert true;
+    }
+}
